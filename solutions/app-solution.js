c26d15297884fd5867b0954cc4edf952b62e468a
Ruth Willenborg <rewillen@us.ibm.com>
2014-07-10 12:18:21 +0000

    added randomword in to manifest

--- solutions/app-solution.js
+++ solutions/app-solution.js
@@ -1,20 +1,14 @@
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-
-// Stuff to do for all routes
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
-/**
  * This is our home route.  This gets called when we visit our
  * base address http://MYSERVERNAME.mybluemix.net/
 **/
 var userCount = 0;
-var userbytwo = 0; /* added var definition for userbytwo here */
+/* added var definition for your new variable, userbytwo here */
+var userbytwo = 0;
 
 app.get('/', function(req, res){
   userCount = userCount + 1;
-  userbytwo = userbytwo + 2; /* added statement to increment userbytwo by two here */
+  /* added statement to increment userbytwo by two here */
+  userbytwo = userbytwo + 2;
   res.render('index', {userCount: userCount, userbytwo: userbytwo}); /* updated this line to also pass userbytwo */
 });
 
